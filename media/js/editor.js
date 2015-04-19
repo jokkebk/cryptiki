3cd6497cc781b31863631b00151371d3ff746fc5
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2015-04-19 08:33:21 +0000

    Rename editor2.js to editor.js

--- media/js/editor.js
+++ media/js/editor.js
@@ -1,266 +1,197 @@
 var Editor = Editor || {};
 
 (function() {
-    var STATE = {
-        BASE: 0, IN_PARAGRAPH: 1, IN_LIST: 2, IN_ENTRY: 3
-    };
+  var STATE = { BASE: 0, IN_PARAGRAPH: 1, IN_LIST: 2, IN_ENTRY: 3 };
 
-    var PATT = {
-        EMTPY: /^\s*$/,
-        HEAD: /^\!(.+)\s*$/,
-        LIST: /^\*\s+(.*\S)\s*$/,
-        ENTRY: /^(\w.*):\s+([^\/]+)\s+\/ (.+)$/,
-        FIELD: /^\s+([^:]+):\s(.+)$/
-    };
+  var PATT = {
+    EMTPY: /^\s*$/,
+    HEAD: /^\!(.+)\s*$/,
+    LIST: /^\*\s+(.*\S)\s*$/, // trim trailing space
+    ENTRY: /^(\w.*):\s+(.+) \/ (.+)$/,
+    NOTE: /^\s+(.*\S)\s*$/ // trim trailing space
+  };
 
-    var TPL = {
-        'heading': doT.template('<h1 id="elem{{=it.id}}" contenteditable="true">{{=it.value}}</h1>'),
-        'list': doT.template(
-            '<ul id="elem{{=it.id}}" contenteditable="true">' +
-                '{{~it.value :value:index}}' +
-                '<li>{{=value}}</li>' +
-                '{{~}}' +
-                '</ul>'
-        ),
-        'paragraph': doT.template('<p id="elem{{=it.id}}" contenteditable="true">{{=it.value.join("<br>")}}</p>'),
-        'entry': doT.template(
-            '<div id="elem{{=it.id}}" class="entry">' +
-                '<div class="name" contenteditable="true">{{=it.name}}</div>' +
-                '<div class="username" contenteditable="true">{{=it.username}}</div>' +
-                '<div class="password" contenteditable="true">{{=it.password}}</div>' +
-                '<div class="tools">' +
-                '<a href="#" class="newfield">New field</a>' +
-                '{{? !$.isEmptyObject(it.value) }}' +
-                ' <a href="#" class="moreless">[+]</a>' +
-                '{{?}}' +
-                '</div>' +
-                '<div style="clear: both;"></div>' +
-                '<div class="fields" style="display: none;">' +
-                '<dl>' +
-                '{{ for(var key in it.value) { }}' +
-                '<dt><img src="/media/icon/delete.png" class="removefield"> <span>{{=key}}</span>:</dt>' +
-                '<dd contenteditable="true">{{=it.value[key]}}</dd>' +
-                '{{ } }}' +
-                '</dl>' +
-                '<div style="clear: both;"></div>' +
-                '</div>' + // fields
-                '</div>'
-        ),
-        'field': doT.template( // should be identical to above except property interpolation
-            '<dt><img src="/media/icon/delete.png" class="removefield"> <span>{{=it.key}}</span>:</dt>' +
-                '<dd contenteditable="true">{{=it.value}}</dd>'
-        )
-    };
+  var TOHTML = {
+    'heading': doT.template('<div class="row"><div class="col-md-12">' +
+      '<h2 id="elem{{=it.id}}" contenteditable="true">{{=it.value}}</h2>' +
+      '</div></div>'),
+    'paragraph': doT.template('<div class="row"><div class="col-md-12">' +
+        '<p id="elem{{=it.id}}" contenteditable="true">{{=it.value.join("<br>")}}</p>' +
+        '</div></div>'),
+    'list': doT.template('<div class="row"><div class="col-md-12">' +
+      '<ul id="elem{{=it.id}}" contenteditable="true">' +
+      '{{~it.value :value:index}}' +
+      '<li>{{=value}}</li>' +
+      '{{~}}' +
+      '</ul>' +
+      '</div></div>'),
+    'entry': doT.template('<div id="elem{{=it.id}}" class="row entry">' +
+        '<div class="col-xs-12 col-md-3"><p class="name bg-primary" contenteditable="true">{{=it.value.name}}</p></div>' +
+        '<div class="col-xs-12 col-sm-6 col-md-3"><p class="username" contenteditable="true">{{=it.value.username}}</p></div>' +
+        '<div class="col-xs-9 col-sm-4 col-md-3"><p class="password" contenteditable="true">{{=it.value.password}}</p></div>' +
+        '<div class="col-xs-3 col-sm-2">' +
+        '<a href="#" class="moreless">' +
+        '{{? it.value.notes.length }}' +
+        '<span class="glyphicon glyphicon-comment text-primary" aria-hidden="true"></span>' +
+        '{{??}}' +
+        '<span class="glyphicon glyphicon-comment" aria-hidden="true"></span>' +
+        '{{?}}' +
+        '</a>' +
+        '</div>' +
+        '<div class="col-xs-12">' +
+        '<blockquote class="notes" contenteditable="true">{{=it.value.notes.join("<br>")}}</blockquote>' +
+        '</div>' +
+        '</div>')
+  };
 
-    function Element(id, type, value) {
-        this.id = id;
-        this.type = type;
-        this.value = value;
-    };
+  var doTopt = $.extend({}, doT.templateSettings, {strip: false});
 
-    Element.prototype.toHtml = function() {
-        if(this.type in TPL)
-            return TPL[this.type](this);
+  var TOTEXT = {
+    'heading': doT.template('!{{=it.value}}\n\n', doTopt),
+    'paragraph': doT.template('{{=it.value.join("\\n")}}\n\n', doTopt),
+    'list': doT.template('{{~it.value :value:index}}* {{=value}}\n{{~}}\n', doTopt),
+    'entry': doT.template('{{=it.value.name}}: {{=it.value.username}} / {{=it.value.password}}\n' +
+        '{{? it.value.notes.length }}  {{=it.value.notes.join("\\n  ")}}\n{{?}}', doTopt)
+  };
 
-        return ''; // unknown type
-    };
+  Editor.Editor = function() {
+    this.clearElements();
+  };
 
-    Element.prototype.toText = function() {
-        switch(this.type) {
-            case 'heading':
-                return '!' + this.value + '\n\n';
-                break;
-            case 'paragraph':
-                return this.value.join('\n') + '\n\n';
-                break;
-            case 'list':
-                return '* ' + this.value.join('\n* ') + '\n\n';
-                break;
-            case 'entry':
-                var text = this.name + ': ' + this.username + ' / ' + this.password + '\n';
-                for(var key in this.value) {
-                    if(this.value.hasOwnProperty(key))
-                        text += '  ' + key + ': ' + this.value[key] + '\n';
-                }
-                //text += '\n';
-                return text;
+  Editor.Editor.prototype.clearElements = function(type, value) {
+    this.elements = [];
+    this.elementMap = {};
+    this.nextId = 0;
+  };
+
+  Editor.Editor.prototype.createElement = function(type, value) {
+    var elem = {id: this.nextId++, type: type, value: value};
+    this.elementMap[elem.id] = elem;
+    this.elements.push(elem.id);
+
+    return elem;
+  };
+
+  // Initialize elements from text
+  Editor.Editor.prototype.fromText = function(text) {
+    var lines = text.split('\n'), i, state = STATE.BASE;
+    var element = null;
+
+    this.clearElements();
+
+    for(i = 0; i < lines.length; i++) {
+      var line = lines[i], match;
+
+      if(PATT.EMTPY.exec(line) !== null) {
+        state = STATE.BASE; // return to base state
+        continue; // go to next line
+      }
+
+      if((match = PATT.HEAD.exec(line)) !== null) {
+        element = this.createElement('heading', match[1]);
+        state = STATE.BASE; // not in para, list, entry any more
+      } else if((match = PATT.LIST.exec(line)) !== null) {
+        if(state != STATE.IN_LIST) { // create new object
+          element = this.createElement('list', []);
+          state = STATE.IN_LIST;
         }
-        return ''; // unknown type
-    };
-
-    Element.prototype.fromHtml = function(elem) {
-        switch(this.type) {
-            case 'heading':
-                this.value = elem.html();
-                break;
-            case 'paragraph':
-                this.value = elem.html().split('<br>');
-                break;
-            case 'list':
-                var list = this.value = [];
-                elem.children('li').each(function(idx, item) {
-                    var html = $(item).html();
-                    html = html.replace('<br>', '');
-                    list.push(html);
-                });
-                break;
-            case 'entry':
-                alert('Not implemented yet!');
-                break;
+        element.value.push(match[1]); // append item
+      } else if((match = PATT.ENTRY.exec(line)) !== null) {
+        element = this.createElement('entry', {
+          name: match[1],
+          username: match[2],
+          password: match[3],
+          notes: []
+        });
+        state = STATE.IN_ENTRY;
+      } else if(state == STATE.IN_ENTRY &&
+          (match = PATT.NOTE.exec(line)) !== null) {
+        element.value.notes.push(match[1]);
+      } else {
+        if(state != STATE.IN_PARAGRAPH) { // create new object
+          element = this.createElement('paragraph', []);
+          state = STATE.IN_PARAGRAPH;
         }
-    };
+        element.value.push(line);
+      }
+    }
+  };
 
-    Editor.Editor = function() {
-        this.clearElements();
-    };
+  Editor.Editor.prototype.toHtml = function() {
+    var html = '', i;
+    for(i = 0; i < this.elements.length; i++) {
+      var elem = this.elementMap[this.elements[i]];
+      html += TOHTML[elem.type](elem);
+    }
+    return html;
+  };
 
-    Editor.Editor.prototype.clearElements = function(type, value) {
-        this.elements = [];
-        this.elementMap = {};
-        this.nextId = 0;
-    };
 
-    Editor.Editor.prototype.createElement = function(type, value) {
-        var elem = new Element(this.nextId++, type, value);
-        this.elementMap[elem.id] = elem;
-        this.elements.push(elem.id);
+  Editor.Editor.prototype.toText = function() {
+    var text = '', i;
+    for(i = 0; i < this.elements.length; i++) {
+      var elem = this.elementMap[this.elements[i]];
+      text += TOTEXT[elem.type](elem);
+    }
+    return text;
+  };
 
-        return elem;
-    };
+  // Normalize webkit <div> linebreaks to <br> and remove all other tags
+  function splitEditable(html) {
+    html = html.replace(/<div>/gi, '\n'); // Assume Webkit line break
+    html = html.replace(/<br>/gi, '\n'); // Firefox et al.
+    html = html.replace(/<[^>]+>/gi, ''); // Remove all other tags
+    return html.split('\n');
+  }
 
-    // Initialize elements from text
-    Editor.Editor.prototype.fromText = function(text) {
-        var lines = text.split('\n'), i, state = STATE.BASE;
-        var element = null;
+  Editor.Editor.prototype.attachListeners = function(elemId) {
+    var me = this;
 
-        this.clearElements();
+    // Watch changes on editable elements
+    $(elemId).on('blur', '[contenteditable]', function(ev) {
+      var id = $(this).attr('id');
 
-        for(i = 0; i < lines.length; i++) {
-            var line = lines[i], match;
-
-            if(PATT.EMTPY.exec(line) !== null) {
-                state = STATE.BASE; // return to base state
-                continue; // go to next line
-            }
-
-            if((match = PATT.HEAD.exec(line)) !== null) {
-                element = this.createElement('heading', match[1]);
-                state = STATE.BASE; // not in para, list, entry any more
-            } else if((match = PATT.LIST.exec(line)) !== null) {
-                if(state != STATE.IN_LIST) { // create new object
-                    element = this.createElement('list', []);
-                    state = STATE.IN_LIST;
-                }
-                element.value.push(match[1]); // append item
-            } else if((match = PATT.ENTRY.exec(line)) !== null) {
-                // create new entry object
-                element = this.createElement('entry', {});
-                element.name = match[1];
-                element.username = match[2];
-                element.password = match[3];
-                state = STATE.IN_ENTRY;
-            } else if(state == STATE.IN_ENTRY &&
-                (match = PATT.FIELD.exec(line)) !== null) {
-                    element.value[match[1]] = match[2]; // set name/value pair
-                } else {
-                    if(state != STATE.IN_PARAGRAPH) { // create new object
-                        element = this.createElement('paragraph', []);
-                        state = STATE.IN_PARAGRAPH;
-                    }
-                    element.value.push(line);
-                }
+      if(id && id.substr(0,4) == 'elem') { // atomic editable elements
+        id = id.substr(4);
+        var elem = me.elementMap[parseInt(id)];
+        switch(elem.type) {
+          case 'heading':
+            $(this).html($(this).text()); // Remove formatting
+            elem.value = $(this).html();
+            break;
+          case 'paragraph':
+            elem.value = splitEditable($(this).html());
+            $(this).html(elem.value.join('<br>'));
+            break;
+          case 'list':
+            elem.value = [];
+            $(this).children('li').each(function(idx, item) {
+              $(item).html($(item).text()); // Remove formatting
+              elem.value.push($(item).text());
+            });
+            break;
         }
-    };
+      } else { // entry
+        var entry = $(this).closest('.entry'), cls = $(this).attr('class');
+        var id = parseInt(entry.attr('id').substr(4));
 
-    Editor.Editor.prototype.toHtml = function() {
-        var html = '', i;
-        for(i = 0; i < this.elements.length; i++)
-            html += this.elementMap[this.elements[i]].toHtml();
-        return html;
-    };
+        if(cls == 'notes') {
+          me.elementMap[id].value.notes = splitEditable($(this).html());
+          $(this).html(me.elementMap[id].value.notes.join('<br>'));
+        } else {
+          $(this).html($(this).text()); // Remove formatting
+          me.elementMap[id].value[cls] = $(this).text();
+        }
+      }
+    });
 
-
-    Editor.Editor.prototype.toText = function() {
-        var text = '', i;
-        for(i = 0; i < this.elements.length; i++)
-            text += this.elementMap[this.elements[i]].toText();
-        return text;
-    };
-
-    Editor.Editor.prototype.attachListeners = function(elemId) {
-        var me = this;
-
-        // Watch changes on editable elements
-        $(elemId).on('blur', '[contenteditable]', function(ev) {
-          var id = $(this).attr('id');
-
-          if(id && id.substr(0,4) == 'elem') { // atomic editable elements
-            id = id.substr(4);
-            me.elementMap[parseInt(id)].fromHtml($(this));
-          } else { // entry
-            var entry = $(this).closest('.entry'), cls = $(this).attr('class'),
-                id = parseInt(entry.attr('id').substr(4));
-
-            switch(cls) {
-              case 'name': case 'username': case 'password':
-                me.elementMap[id][cls] = $(this).html().replace('<br>', '');
-                break;
-              default:
-                me.elementMap[id].value[$(this).prev().html()] =
-                  $(this).html().replace('<br>', ' ');
-                break;
-            }
-          }
-        });
-
-        $(elemId).on('click', '.moreless', function(ev) {
-          var field = $(this).closest('.entry').children('.fields');
-
-          if(field.is(':visible')) {
-            field.hide(400);
-            $(this).html('[+]');
-          } else {
-            field.show(400);
-            $(this).html('[-]');
-          }
-
-          return false;
-        });
-
-        $(elemId).on('click', '.removefield', function(ev) {
-          var field = $(this).next().html(), parent = $(this).parent(),
-              entry = $(this).closest('.entry'),
-              id = parseInt(entry.attr('id').substr(4));
-
-          parent.next().remove(); // remove dd
-          parent.remove(); // remove dt
-          delete me.elementMap[id].value[field]; // remove data
-        });
-
-        $(elemId).on('click', '.newfield', function(ev) {
-          var entry = $(this).closest('.entry'), dl = entry.find('dl'),
-              id = parseInt(entry.attr('id').substr(4)),
-              name = prompt('Give field a name');
-
-          if(!name || name === '') return false;
-
-          if(name in me.elementMap[id].value) {
-            alert(name + ' already exists!');
-          } else {
-            var value = prompt('Give value for field');
-
-            if(value && value !== '') {
-              if($.isEmptyObject(me.elementMap[id].value)) { // first element
-                me.elementMap[id].value[name] = value; // add name/value field
-                entry.replaceWith(TPL.entry(me.elementMap[id]));
-              } else { // existing elements, and thus [+] / [-]
-                me.elementMap[id].value[name] = value; // add name/value field
-                dl.append(TPL.field({key: name, value: value}));
-              }
-            }
-          }
-
-          return false;
-        });
-    };
+    $(elemId).on('click', '.moreless', function(ev) {
+      var field = $(this).closest('.entry').find('.notes');
+      if(field.is(':visible'))
+        field.hide(400);
+      else
+        field.show(400);
+      return false;
+    });
+  };
 })();
